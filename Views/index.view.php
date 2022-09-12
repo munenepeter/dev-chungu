2cdb8a6d9dd8df91a8cc35e11e74ae70d72f7948
munenepeter <peternjega7@gmail.com>
2022-09-12 17:28:19 +0300

    reformat

--- Views/index.view.php
+++ Views/index.view.php
@@ -3,7 +3,6 @@ include_once 'sections/nav.view.php';
 ?>
 
 <div class="container mx-auto p-8 overflow-hidden md:rounded-lg md:p-10 lg:p-12">
-
     <section class="bg-white dark:bg-gray-900">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
