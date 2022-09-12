71ae6368b34533e5de47c5083a445fc6e4be1778
munenepeter <peternjega7@gmail.com>
2022-09-12 12:47:00 +0300

    Add support for logs

--- routes.php
+++ routes.php
@@ -4,8 +4,11 @@
 $router->get('', 'PagesController@index');
 $router->get('projects', 'PagesController@shop');
 
-//robots
 
+
+//logs
+$router->get('logs', 'SystemController@index');
+//robots
 $router->get('robots.txt', function (){
     return require __DIR__ ."/robots.txt";
 });
