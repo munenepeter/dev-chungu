406031d012921d764eb57233f29d422b553d2e18
munenepeter <peternjega7@gmail.com>
2022-09-12 16:16:17 +0300

    refreshed UI

--- Views/sections/nav.view.php
+++ Views/sections/nav.view.php
@@ -1,4 +1,4 @@
-<nav class="bg-white">
+<nav class="bg-white shadow">
     <div class="container flex flex-wrap justify-between items-center mx-auto p-6">
         <div class="text-4xl font-extrabold tracking-tight">Chungu Devs</div>
 
@@ -6,12 +6,10 @@
             <li>Home</li>
             <li>About Us</li>
             <li>Projects</li>
-            <li>Contact</li>
 
         </ul>
 
-        <div class="inline-flex items-center justify-center px-3 py-2 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 ">Get a Qoute</div>
-
+        <div class="inline-flex items-center justify-center px-3 py-2 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 ">Hire Us</div>
     </div>
 </nav>
 
@@ -24,13 +22,13 @@
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Your Ultimate Reliable developer</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From native apps to websites, companies around the world contract Chungu to develop their dreams and needs.</p>
             <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                Get started
+                Services
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                 </svg>
             </a>
             <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 ">
-                Get Qoute
+                Hire Us
             </a>
         </div>
         <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
