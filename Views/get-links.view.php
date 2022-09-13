e1c44a88b18fd0db86d99353f7e78b183c7a4837
munenepeter <peternjega7@gmail.com>
2022-09-13 15:02:27 +0300

    done with get-links

--- Views/get-links.view.php
+++ Views/get-links.view.php
@@ -1,21 +1,64 @@
-<?php include_once 'base.view.php';?>
+<?php
+
+use spekulatius\phpscraper;
+
+include_once 'base.view.php'; ?>
+<h1 class="mb-2 text-center text-2xl font-bold tracking-tight">Get Links, useful tool to get all the urls of a site</h1>
+
+<div class="mt-4 p-6 mx-auto max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+       <h1 class="mb-2 text-2xl font-bold tracking-tight">
+              <?php if (isset($_GET['error'])) : ?>
+                     <span class="text-red-500"><?= $_GET['error'] ?>
+                     </span>
+              <?php endif; ?>
+       </h1>
+       <form action="" method="get">
+              <div class="mb-6">
+                     <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Input your URL</label>
+                     <input type="url" name="url" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+              </div>
+              <button name="submit" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Search</button>
+       </form>
+</div>
+
+
+<?php
+if (isset($_GET['submit'])) {
+       if (empty($_GET['url'])) {
+              $error = urlencode("No URL was provided!!");
+              header("Location:index.php?error=$error");
+       }
+       $start = microtime(true);
+       $web = new phpscraper();
+       $links = [];
+       try {
+              $web->go($_GET['url']);
+              foreach ($web->links as $link) {
+                     array_push($links, $link . "\n");
+              }
+              $host = parse_url($_GET['url'], PHP_URL_HOST);
+              $time = round(microtime(true) - $start, 2);
+       } catch (\Exception $e) {
+              echo "<h1 class='mb-2 text-2xl font-bold tracking-tight text-red-500'>" . $e->getMessage() . "</h1> ";
+       }
+
+?>
+       <div>
+              <p class="text-lg m-4"><?= "Done getting " . count($web->links) . " links for $host in $time Seconds"; ?></p>
+
+              <div class="border m-4 p-2 rounded-md">
+                     <?php $count = 1; ?>
+                     <?php foreach ($links as $link) : ?>
+                            <p class='font-normal text-gray-700'><?= $count . ".  "; ?><a href="<?= $link; ?>"><?= $link; ?></a></p>
+                            <?php $count++;?>
+                     <?php endforeach; ?>
+              </div>
+       </div>
 
 
 
 
+<?php
 
-<div class="p-6 mx-auto max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <h1 class="mb-2 text-2xl font-bold tracking-tight">
-                <?php if (isset($_GET['error'])) : ?>
-                    <span class="text-red-500"><?= $_GET['error'] ?>
-                    </span>
-                <?php endif; ?>
-            </h1>
-            <form action="" method="get">
-                <div class="mb-6">
-                    <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Input your URL</label>
-                    <input type="url" name="url" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-                </div>
-                <button name="submit" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Go</button>
-            </form>
-        </div>
+}
+?>
