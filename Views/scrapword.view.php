bdc8ec268b19a30246764a296ae65832406e9727
munenepeter <peternjega7@gmail.com>
2022-09-16 17:38:50 +0300

    more informative

--- Views/scrapword.view.php
+++ Views/scrapword.view.php
@@ -71,7 +71,7 @@ include_once 'sections/nav.view.php';
         </h1>
         <form action="" method="get">
             <div class="mb-6">
-                <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Input your URL</label>
+                <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Input URL to website or a PDF file</label>
                 <input type="url" name="url" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
             </div>
             <button name="submit" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Go</button>
