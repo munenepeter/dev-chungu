93f395f07dd2663b0b9f9b378227c0f2f58f7a90
munenepeter <peternjega7@gmail.com>
2022-09-25 14:42:09 +0300

    done with the more part

--- Views/scrapword.view.php
+++ Views/scrapword.view.php
@@ -5,19 +5,37 @@ use Smalot\PdfParser\Parser;
 include_once 'base.view.php';
 include_once 'sections/nav.view.php';
 ?>
-
+<?php $st_keywords = []; ?>
+<?php foreach ($s_keyWords as $keyWord) : ?>
+    <?php $keyWord = json_decode($keyWord); ?>
+    <?php array_push($st_keywords, strtolower($keyWord->word)); ?>
+<?php endforeach; ?>
 
 <div class="space-y-4 p-4 mt-6 ">
     <div class="flex justify-between">
-        <p class="mt-2 mx-auto">
+        <div class="mt-2 mx-auto">
             Searching for the following Keywords: <br><br>
-            <?php $st_keywords = []; ?>
-            <?php foreach ($s_keyWords as $keyWord) : ?>
+            <?php foreach (array_slice($s_keyWords, 0, 11) as $keyWord) : ?>
                 <?php $keyWord = json_decode($keyWord); ?>
-                <?php array_push($st_keywords, strtolower($keyWord->word)); ?>
-                <button style="background-color:<?= $keyWord->color; ?>;" type="button" class="text-white focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-2 py-1 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><?= $keyWord->word; ?></button>
+                <button style="background-color:<?= $keyWord->color; ?>;" type="button" class="text-white focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-2 py-1 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><?= ucfirst($keyWord->word); ?></button>
             <?php endforeach; ?>
-        </p>
+            <div x-data="{ keywords: false }">
+                <button @click.prevent="keywords = true" type="button" class="bg-green-700 text-white focus:outline-none focus:ring-4 focus:ring-blue-300 font-semibold italic rounded-full text-sm px-2 py-1 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">+<?= count($st_keywords) - 11; ?> more</button>
+                <template x-if="keywords">
+                    <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center z-10" style="background-color: rgba(0,0,0,.5);">
+                        <div class="bg-green-50 h-auto p-2 md:max-w-screen-lg md:p-2 lg:p-4 shadow-xl rounded mx-2 md:mx-0" @click.away="open = false">
+                        <button @click="keywords = false" type="button" class="text-right mt-2 w-10 inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base mb-2 ">X</button>
+                        <div class="text-left  mt-4">
+                            <?php foreach ($s_keyWords as $keyWord) : ?>
+                                <?php $keyWord = json_decode($keyWord); ?>
+                                <button style="background-color:<?= $keyWord->color; ?>;" type="button" class="text-white focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-2 py-1 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><?= ucfirst($keyWord->word); ?></button>
+                            <?php endforeach; ?>
+                            </div>
+                        </div>
+                    </div>
+                </template>
+            </div>
+        </div>
 
         <div x-data="{ open: false }">
             <button @click.prevent="open = true" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Keyword</button>
