7d82e64ef126bb51f1e804b57d356b826b10da7a
munenepeter <peternjega7@gmail.com>
2022-10-05 23:42:38 +0300

    demo fun

--- Controllers/DevsController.php
+++ Controllers/DevsController.php
@@ -4,5 +4,26 @@ namespace Chungu\Controllers;
 
 
 class DevsController {
-    
+
+    private function json(...$values) {
+        return json_encode($values);
+    }
+    public function index() {
+        return $this->json(["message" => "Success"]);
+    }
+    public function users() {
+        # code...
+    }
+    public function show($id) {
+        # code...
+    }
+    public function signin() {
+        # code...
+    }
+    public function update($id) {
+        # code...
+    }
+    public function delete($id) {
+        # code...
+    }
 }
