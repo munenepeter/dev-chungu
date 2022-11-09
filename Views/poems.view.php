69c22bf38cdcdc68172fd0cb63402bece153c161
munenepeter <peternjega7@gmail.com>
2022-11-09 13:40:15 +0300

    done with initial look

--- Views/poems.view.php
+++ Views/poems.view.php
@@ -3,8 +3,33 @@ include_once 'base.view.php';
 include_once 'sections/nav.view.php';
 ?>
 <script src="https://cdn.tailwindcss.com"></script>
+<script>
+       tailwind.config = {
+              theme: {
+                     extend: {
+                            colors: {
+                                   mahogany: '#da373d',
+                            }
+                     }
+              }
+       }
+</script>
 <div class="grid place-items-center text-center">
-       Comming Soon
+       <div class="container mx-auto my-12 px-4 sm:px-0">
+              <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                     <?php for ($i = 0; $i < 22; $i++) : ?>
+                            <div class="flex justify-center border-2 border-rose-200 rounded-xl p-6 bg-rose-50">
+                                   <article>
+                                          <h1 class=" text-5xl text-rose-600">Title</h1>
+                                          <p class="pt-4 text-md text-rose-300">Lorem ipsum dolor, sit amet consectetur adipisi....</p>
+                                   </article>
+                            </div>
+                     <?php endfor; ?>
+
+              </div>
+       </div>
+</div>
+
 </div>
 
 <div class="border-t bg-gray-50 left-50 w-full  bottom-0" style="position: fixed;  left: 50%; transform: translate(-50%, 0);">
