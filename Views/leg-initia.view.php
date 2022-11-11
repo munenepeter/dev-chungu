01128c726749edb44a8d35e8c0f443b41cfc5dfc
munenepeter <peternjega7@gmail.com>
2022-11-11 15:50:08 +0300

    Add LI Modal

--- Views/leg-initia.view.php
+++ Views/leg-initia.view.php
@@ -27,9 +27,61 @@ include_once 'sections/nav.view.php';
               </div>
               <div class="bg-white shadow-xl pl-4 pb-2 pt-4 rounded-xl mt-10">
                      <div class="flex items-center justify-between font-semibold border-b border-gray-100 space-x-16 md:space-x-64 ">
-                            <button type="button" class="mb-2 inline-flex justify-center rounded-md px-1 -mr-1 bg-white text-sm leading-5 font-medium text-blue-500 hover:text-gray-600" id="options-menu" aria-haspopup="true" aria-expanded="true">
+                     <section>
+                     <button id="defaultModalButton" data-modal-toggle="defaultModal" type="button" class="mb-2 inline-flex justify-center rounded-md px-1 -mr-1 bg-white text-sm leading-5 font-medium text-blue-500 hover:text-gray-600" id="options-menu" aria-haspopup="true" aria-expanded="true">
                                    Add LI
                             </button>
+
+
+                            <!-- Main modal -->
+                            <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                                   <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+                                          <!-- Modal content -->
+                                          <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                                                 <!-- Modal header -->
+                                                 <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
+                                                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                                               Add a new legislative initiative
+                                                        </h3>
+                                                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                                                               <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                                      <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                                               </svg>
+                                                               <span class="sr-only">Close modal</span>
+                                                        </button>
+                                                 </div>
+                                                 <!-- Modal body -->
+                                                 <form action="#" >
+                                                        <div class="grid gap-4 mb-4 sm:grid-cols-2">
+                                                               <div class="sm:col-span-2">
+                                                                      <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Name</label>
+                                                                      <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-rose-600 focus:border-rose-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-rose-500 dark:focus:border-rose-500" placeholder="Type your name" required="">
+                                                               </div>
+                                                              
+                                                               <div class="sm:col-span-2">
+                                                                      <label for="liname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Legislative initiative Name</label>
+                                                                      <input type="text" name="liname" id="liname" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-rose-600 focus:border-rose-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-rose-500 dark:focus:border-rose-500" placeholder="EBA Regulation" required="">
+                                                               </div>
+                                                               
+                                                               <div class="sm:col-span-2">
+                                                                      <label for="liabbr" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Legislative initiative Abbrevations</label>
+                                                                      <textarea id="liabbr" name="liabbr" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-rose-500 focus:border-rose-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-rose-500 dark:focus:border-rose-500" placeholder="Write your LI's pearl terms or abbrevations e.g. 1093/2010 (separated with a comma)"></textarea>
+                                                               </div>
+                                                        </div>
+                                                        <button type="submit" class="text-white inline-flex items-center bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:outline-none focus:ring-rose-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800">
+                                                               <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                                      <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
+                                                               </svg>
+                                                               Add legislative initiative
+                                                        </button>
+                                                 </form>
+                                          </div>
+                                   </div>
+                            </div>
+                          
+
+                            </section>
+
                             <button type="button" class="mb-2 inline-flex justify-center rounded-md px-1 -mr-1 bg-white text-sm leading-5 font-medium text-blue-500 hover:text-gray-600" id="options-menu" aria-haspopup="true" aria-expanded="true">
                                    View Log
                             </button>
