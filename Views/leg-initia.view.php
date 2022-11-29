eba3941e7ebe662ca56bdf25a392222076914b74
munenepeter <peternjega7@gmail.com>
2022-11-29 11:47:59 +0300

    Debug#2

--- Views/leg-initia.view.php
+++ Views/leg-initia.view.php
@@ -17,8 +17,8 @@ include_once 'sections/nav.view.php';
        }
 </style>
 <?php
-
-dd(Chungu\Models\Li::all())
+echo count(Chungu\Models\Li::all());
+dd(Chungu\Models\Li::all());
 ?>
 <div class="grid place-items-center bg-gray-100" id="main">
        <div class="max-w-sm md:max-w-lg bg-gray-100 px-4 md:px-8 py-14">
