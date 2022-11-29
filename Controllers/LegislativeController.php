987ae9a50983625a544c956bfe4d7d06e0483b03
munenepeter <peternjega7@gmail.com>
2022-11-29 12:07:35 +0300

    reformat

--- Controllers/LegislativeController.php
+++ Controllers/LegislativeController.php
@@ -4,11 +4,11 @@ namespace Chungu\Controllers;
 
 use Chungu\Models\Li;
 
-class LegislativeController extends Controller{
+class LegislativeController extends Controller {
     public function index() {
         return view('leg-initia');
     }
-    public function allLis(){
+    public function allLis() {
         echo $this->json("Ok", Li::all());
     }
 }
