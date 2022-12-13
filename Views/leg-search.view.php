da5d4c5d0fc1e9fdab0cb91471fdbe71ed80822f
munenepeter <peternjega7@gmail.com>
2022-12-13 15:56:59 +0300

    done with the UI

--- Views/leg-search.view.php
+++ Views/leg-search.view.php
@@ -7,30 +7,19 @@ include_once 'sections/nav.view.php';
 ?>
 
 
-<div class="space-y-2 p-4">
-    <p class="md:hidden text-green-500 font-semibold">Searching for Keywords</p>
-    <div class="hidden md:flex justify-between">
-        <div class="mt-2 mx-auto">
-            Searching for the following Keywords: <br><br>
-        </div>
-    </div>
-
-    <div class="p-6 mx-auto max-w-xl bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <h1 class="mb-2 text-2xl font-bold tracking-tight">
-        </h1>
+<div class="p-4">
+    <div class="space-y-2 p-2  min-w-xl bg-white  dark:bg-gray-800 dark:border-gray-700">
+        <p class=" text-green-500 font-semibold">Searching for Keywords</p>
         <form action="" method="get">
-            <div class="mb-6">
-                <div contenteditable="true" class="border h-64">
-
+            <div class="mb-2">
+                <div contenteditable="true" class="h-96 p-2 overflow-y-auto rounded-lg border border-gray-200 shadow-md">
+                    <span style="color: #aaa;">Simply paste your rich-text content in here or scroll down to learn more</span>
                 </div>
-                <div class="my-4 text-sm text-left text-gray-500">Only links to actual websites and links to pdf docs will work</div>
+                <div class="my-1 text-sm text-left text-gray-500">Only links to actual websites and links to pdf docs will work</div>
             </div>
-           <div class="flex justify-end">
-           <button name="submit" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Search</button>
-           </div>
-               
-
-      
+            <div class="flex justify-end">
+                <button name="submit" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Search</button>
+            </div>
         </form>
     </div>
 
