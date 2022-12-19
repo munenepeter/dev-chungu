717167395256c729bacf196ca164c49555de6d76
munenepeter <peternjega7@gmail.com>
2022-12-19 14:29:42 +0300

    Added indicators

--- Views/leg-initia.view.php
+++ Views/leg-initia.view.php
@@ -75,6 +75,19 @@ include_once 'sections/nav.view.php';
                                                                       </svg>
                                                                       Add legislative initiative
                                                                </button>
+                                                               <button id="loading" disabled type="button" class="hidden text-white bg-rose-700 hover:bg-rose-800 focus:ring-4 focus:ring-rose-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-rose-600 dark:hover:bg-rose-700 dark:focus:ring-rose-800 inline-flex items-center">
+                                                                      <svg role="status" class="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                             <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                                                                             <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                                                                      </svg>
+                                                                      Loading...
+                                                               </button>
+                                                               <button id='success' type="submit" class="hidden text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 cursor-not-allowed" disabled>
+                                                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-1 -ml-1 w-6 h-6">
+                                                                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                                      </svg>
+                                                                      Success
+                                                               </button>
                                                         </form>
                                                  </div>
                                           </div>
@@ -214,10 +227,14 @@ include_once 'sections/nav.view.php';
 
 
        // Get the form element
-       const btn = document.getElementById("addLiBtn");
+       const addBtn = document.getElementById("addLiBtn");
+       const loadBtn = document.getElementById("loading");
+       const successBtn = document.getElementById("success");
 
        // Attach an event listener to the form's submit event
-       btn.addEventListener("click", function(event) {
+       addBtn.addEventListener("click", function(event) {
+              addBtn.classList.add("hidden");
+              loadBtn.classList.remove("hidden");
               // Prevent the default submit action
               event.preventDefault();
               const form = document.getElementById("addLi");
@@ -229,6 +246,8 @@ include_once 'sections/nav.view.php';
                      .then(function(response) {
                             // Handle the successful response from the server
                             console.log(response);
+                            loadBtn.classList.add("hidden");
+                            successBtn.classList.remove("hidden");
                      })
                      .catch(function(error) {
                             // Handle any errors that occurred during the request
