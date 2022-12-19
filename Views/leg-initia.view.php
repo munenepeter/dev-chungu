7c9aa6c1ce1e6c03d57861d4724c95eddbec49e4
munenepeter <peternjega7@gmail.com>
2022-12-19 14:45:21 +0300

    all good for nw

--- Views/leg-initia.view.php
+++ Views/leg-initia.view.php
@@ -101,7 +101,7 @@ include_once 'sections/nav.view.php';
                             </button>
                      </div>
                      <section id="lis" class="overflow-y-auto h-80 scrollbar-hide">
-                            <div v-for="LI in filteredData" class="pt-4 border-b border-gray-100 flex justify-between group/item hover:bg-gray-50">
+                            <div v-for="LI in filteredData" :key="LI.id" class="pt-4 border-b border-gray-100 flex justify-between group/item hover:bg-gray-50">
                                    <div id="lidata" class="cursor-pointer">
                                           <p class="text-sm font-medium text-rose-900" v-html="LI.name"></p>
                                           <span class="text-sm text-rose-500" v-html="LI.abbr"></span>
@@ -110,10 +110,11 @@ include_once 'sections/nav.view.php';
                                    </div>
                                    <div id="icons" class="invisible group-hover/item:visible flex flex-col justify-center -mt-4 bg-gray-100 rounded-l-full p-4  space-y-4">
                                           <span>
-                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-green-500 cursor-pointer w-4 h-4 hover:text-green-600 hover:w-5 hover:h-5">
+                                               <svg id="deleteButton" data-modal-toggle="deleteModal"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-green-500 cursor-pointer w-4 h-4 hover:text-green-600 hover:w-5 hover:h-5">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                                                 </svg>
+                                                 </svg>    
                                           </span>
+                                    
                                           <span>
                                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-red-500 cursor-pointer w-4 h-4 hover:text-red-600 hover:w-5 hover:h-5">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
