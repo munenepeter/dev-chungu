5b8b03fd9da2bcd44e7cd3e8b06c0328c089f5bc
munenepeter <peternjega7@gmail.com>
2022-12-21 16:00:42 +0300

    Moving to PHP

--- Views/leg-initia.view.php
+++ Views/leg-initia.view.php
@@ -17,7 +17,61 @@ include_once 'sections/nav.view.php';
        }
 </style>
 
-<div class="grid place-items-center bg-gray-100" id="main">
+<section id="lis" class="overflow-y-auto h-80 scrollbar-hide">
+   <?php for ($i=0; $i < 5; $i++):?>
+       <div v-for="LI in filteredData" :key="LI.id" class="pt-4 border-b border-gray-100 flex justify-between group/item hover:bg-gray-50">
+              <div id="lidata" class="cursor-pointer">
+                     <p class="text-sm font-medium text-rose-900" v-html="LI.name"></p>
+                     <span class="text-sm text-rose-500" v-html="LI.abbr"></span>
+                     <br>
+                     <span class="text-xs text-rose-300" v-html="timeSince(LI.updated_at) + '  <b>click to copy</b>'"></span>
+              </div>
+              <div id="icons" class="invisible group-hover/item:visible flex flex-col justify-center -mt-4 bg-gray-100 rounded-l-full p-4  space-y-4">
+                     <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-green-500 cursor-pointer w-4 h-4 hover:text-green-600 hover:w-5 hover:h-5">
+                                   <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                            </svg>
+                     </span>
+
+
+                     <section>
+                            <svg id="deleteButton<?=$i?>" data-modal-toggle="deleteModal<?=$i?>" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-red-500 cursor-pointer w-4 h-4 hover:text-red-600 hover:w-5 hover:h-5">
+                                   <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                            </svg>
+                            <div id="deleteModal<?=$i?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                                   <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+                                          <!-- Modal content -->
+                                          <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                                                 <button type="button" class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="deleteModal<?=$i?>">
+                                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                               <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                                        </svg>
+                                                        <span class="sr-only">Close modal</span>
+                                                 </button>
+                                                 <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                                                 </svg>
+                                                 <p class="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want to delete this item?</p>
+                                                 <div class="flex justify-center items-center space-x-4">
+                                                        <button data-modal-toggle="deleteModal<?=$i?>" type="button" class="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                                                               No, cancel
+                                                        </button>
+                                                        <button type="submit" class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                                                               Yes, I'm sure
+                                                        </button>
+                                                 </div>
+                                          </div>
+                                   </div>
+                            </div>
+                     </section>
+              </div>
+       </div>
+       <?php endfor;?>
+</section>
+
+
+
+<!-- <div class="grid place-items-center bg-gray-100" id="main">
        <div class="max-w-sm md:max-w-lg bg-gray-100 px-4 md:px-8 py-14">
               <div class="relative bg-white flex-auto border-none rounded-xl ">
                      <input type="search" aria-label="Search all lis" placeholder="Search all Legislative initiatives..." class="block w-full appearance-none border-none  rounded-xl bg-transparent  pr-4 pl-10 text-base text-slate-900 transition placeholder:text-slate-400 focus:outline-none focus:ring-0" v-model="searchString">
@@ -34,12 +88,12 @@ include_once 'sections/nav.view.php';
                                    </button>
 
 
-                                   <!-- Main modal -->
+                                 
                                    <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                                           <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-                                                 <!-- Modal content -->
+                                             
                                                  <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-                                                        <!-- Modal header -->
+                                                     
                                                         <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
                                                                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
                                                                       Add a new legislative initiative
@@ -51,7 +105,7 @@ include_once 'sections/nav.view.php';
                                                                       <span class="sr-only">Close modal</span>
                                                                </button>
                                                         </div>
-                                                        <!-- Modal body -->
+                                                       
                                                         <form id="addLi" action="/projects/jwg/leg-initia" method="POST">
                                                                <div class="grid gap-4 mb-4 sm:grid-cols-2">
                                                                       <div class="sm:col-span-2">
@@ -102,12 +156,12 @@ include_once 'sections/nav.view.php';
                                    </button>
 
 
-                                   <!-- Main modal -->
+                                  
                                    <div id="readProductModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                                           <div class="relative p-4 w-full max-w-xl h-full md:h-auto">
-                                                 <!-- Modal content -->
+                                               
                                                  <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-                                                        <!-- Modal header -->
+                                                     
                                                         <div class="flex justify-between mb-4 rounded-t sm:mb-5">
                                                                <div class="text-lg text-gray-900 md:text-xl dark:text-white">
                                                                       <?php
@@ -154,14 +208,14 @@ include_once 'sections/nav.view.php';
 
 
                                           <section>
-                                                 <svg v-bind:id="'deleteButton-'+LI.id" v-bind:data-modal-toggle="'deleteModal-'+LI.id" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-red-500 cursor-pointer w-4 h-4 hover:text-red-600 hover:w-5 hover:h-5">
+                                                 <svg id="deleteButton<?=$i?>" data-modal-toggle="deleteModal<?=$i?>" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-red-500 cursor-pointer w-4 h-4 hover:text-red-600 hover:w-5 hover:h-5">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                  </svg>
-                                                 <div v-bind:id="'deleteModal-'+LI.id" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                                                 <div id="deleteModal<?=$i?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                                                         <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-                                                               <!-- Modal content -->
+                                                               
                                                                <div class="relative p-4 text-center bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
-                                                                      <button type="button" class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" v-bind:data-modal-toggle="'deleteModal-'+LI.id">
+                                                                      <button type="button" class="text-gray-400 absolute top-2.5 right-2.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="deleteModal<?=$i?>">
                                                                              <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                                                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
                                                                              </svg>
@@ -172,7 +226,7 @@ include_once 'sections/nav.view.php';
                                                                       </svg>
                                                                       <p class="mb-4 text-gray-500 dark:text-gray-300">Are you sure you want to delete this item?</p>
                                                                       <div class="flex justify-center items-center space-x-4">
-                                                                             <button v-bind:data-modal-toggle="'deleteModal-'+LI.id" type="button" class="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                                                                             <button data-modal-toggle="deleteModal<?=$i?>" type="button" class="py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
                                                                                     No, cancel
                                                                              </button>
                                                                              <button type="submit" class="py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
@@ -193,7 +247,7 @@ include_once 'sections/nav.view.php';
               </div>
        </div>
 
-</div>
+</div> -->
 <!-- Modal toggle -->
 
 
