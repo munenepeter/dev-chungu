ab25506366911d5abb840c98a8bb1cda3e6b710f
munenepeter <peternjega7@gmail.com>
2023-01-04 16:55:33 +0300

    revamp nav ui

--- Views/sections/nav.view.php
+++ Views/sections/nav.view.php
@@ -1,49 +1,35 @@
-<nav class="bg-rose-50">
-  <div class=" px-4">
-    <div class="flex justify-between">
-
-      <div class="flex space-x-4">
-        <!-- logo -->
-        <div>
-          <a href="#" class="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900">
-            <svg class="h-6 w-6 mr-1 text-blue-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
-            </svg>
-            <span class="font-bold">
-            <span style="font-family: 'Cedarville Cursive', cursive;">Chungu </span>Devs</span>
-          </a>
-        </div>
-
-        <!-- primary nav -->
-        <div class="hidden md:flex items-center space-x-1">
-          <a href="/" class="py-5 px-3 text-gray-700 hover:text-gray-900">Home</a>
-          <a href="/services" class="py-5 px-3 text-gray-700 hover:text-gray-900">Services</a>
-          <a href="/projects" class="py-5 px-3 text-gray-700 hover:text-gray-900">Projects</a>
-          <a href="/about-us" class="py-5 px-3 text-gray-700 hover:text-gray-900">About</a>
-        </div>
-      </div>
-
-      <!-- secondary nav -->
-      <div class="hidden md:flex items-center space-x-1">
-        <a href="" class="py-2 px-3 bg-rose-400 hover:bg-rose-300 text-rose-900 hover:text-rose-800 rounded transition duration-300">Hire Us</a>
-      </div>
-
-      <!-- mobile button goes here -->
-      <div class="md:hidden flex items-center">
-        <button class="mobile-menu-button">
-          <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-      </div>
-
+<nav class="bg-rose-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+  <div class="container flex flex-wrap items-center justify-between mx-auto">
+    <div>
+      <a href="/" class="flex items-center py-2.5 px-2 text-gray-700 hover:text-gray-900">
+        <svg class="h-5 w-5 mr-1 text-blue-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+        </svg>
+        <span class="font-bold">
+          <span style="font-family: 'Cedarville Cursive', cursive;">Chungu </span>Devs</span>
+      </a>
+    </div>
+    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <span class="sr-only">Open main menu</span>
+      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+      </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="bg-rose-50  flex flex-col p-2 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="/about-us" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        </li>
+        <li>
+          <a href="/services" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+        </li>
+        <li>
+          <a href="/projects" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-blue-700 font-semibold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Hire us</a>
+        </li>
+      </ul>
     </div>
   </div>
-
-  <!-- mobile menu -->
-  <div class="mobile-menu hidden md:hidden">
-    <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Features</a>
-    <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Pricing</a>
-  </div>
 </nav>
-
