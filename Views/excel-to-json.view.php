4e77b7d2c954876e2730a6aca88a15479b6865c9
munenepeter <peternjega7@gmail.com>
2023-03-29 16:16:18 +0300

    update theme colors

--- Views/excel-to-json.view.php
+++ Views/excel-to-json.view.php
@@ -7,20 +7,19 @@ include_once 'sections/nav.view.php';
 <body>
 
 
-    <main class="pt-4 pb-10 lg:pt-16 lg:pb-8">
+    <main class="mt-4 pb-10 lg:pt-16 lg:pb-8">
         <section class="flex justify-between px-2 mx-auto max-w-screen-2xl ">
             <article class="mx-auto w-full max-w-4xl format format-sm sm:format-base lg:format-lg format-purple dark:format-invert">
-
-                <div class=" p-2 rounded-lg bg-gray-100 dark:bg-gray-800">
-                    <p id="doc-label" class="text-lg text-center font-medium text-gray-900 dark:text-white">Welcome
+                <div class=" p-2 rounded-lg bg-purple-100">
+                    <p id="doc-label" class="text-lg text-center font-semibold text-purple-900">Welcome
                         to Excel-JSON</p>
                      <p class="block mb-2 text-sm text-center text-gray-500">Select document to convert</p>
-                    <form method="post" enctype="multipart/form-data" id="fileForm" class="flex flex-row justify-center items-center">
+                      <form method="post" enctype="multipart/form-data" id="fileForm" class="flex flex-row justify-center items-center">
                         <!-- form -->
-                        <input class="mr-2 block w-3/4 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" type="file" id="inputfile" accept=".xls,.xlsx" />
-                        <button type="submit" id="convert" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Convert</button>
+                        <input class="mr-2 block w-3/4 text-sm text-purple-700 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" type="file" id="inputfile" accept=".xls,.xlsx" />
+                        <button type="submit" id="convert" class="focus:outline-none text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Convert</button>
                         <!-- status buttons -->
-                        <button id="loading" disabled type="button" class="hidden text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2  inline-flex items-center">
+                        <button id="loading" disabled type="button" class="hidden text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2  inline-flex items-center">
                             <svg role="status" class="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
                                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
@@ -132,7 +131,7 @@ include_once 'sections/nav.view.php';
         }
     </script>
     <div class="border-t bg-gray-50 left-50 w-full  bottom-0" style="position: fixed;  left: 50%; transform: translate(-50%, 0);">
-        <div class="text-gray-900 text-sm text-center">
+        <div class="text-purple-700 text-sm text-center">
             <div class="my-5 text-center">&copy; 2020 - <?= date('Y') ?> All rights reserved | Chungu Developers</div>
         </div>
     </div>
