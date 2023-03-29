ef0014ac0d1912d7f5039ce3eda5f651e26c1b20
munenepeter <peternjega7@gmail.com>
2023-03-29 15:46:32 +0300

    upde from phone to mail

--- Views/sections/nav.view.php
+++ Views/sections/nav.view.php
@@ -1,14 +1,19 @@
 <nav class=" px-2 sm:px-4 py-2 bg-purple-100 fixed w-full  top-0 left-0">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
     <div class="flex items-center space-x-2">
-      <div class="h-8 w-8 bg-orange-200 rounded-full">
+      <!-- <div class="h-8 w-8 bg-orange-200 rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full p-1 text-orange-700">
           <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+        </svg>
+      </div> -->
+      <div class="h-8 w-8 bg-orange-200 rounded-full">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full p-1 text-orange-700">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
         </svg>
       </div>
       <div class="flex flex-col">
         <span style="font-size: 10px;" class="text-purple-900">Hotline 24/7</span>
-        <span class="text-sm text-purple-900 font-semibold">+254 798 055244</span>
+        <span class="text-sm text-purple-900 font-semibold">developers@chungu.co.ke</span>
       </div>
 
     </div>
@@ -21,16 +26,13 @@
     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
       <ul class="flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium ">
         <li>
-          <a href="/" class="block py-2 pl-3 pr-4 text-orange-700 md:p-0" aria-current="page">Dev Chungu</a>
+          <a href="/" class="block py-2 pl-3 pr-4 text-orange-700 md:p-0" aria-current="page">Home</a>
         </li>
         <li>
           <a href="/projects" class="block py-2 pl-3 pr-4 text-purple-700 md:hover:text-orange-700 md:p-0 ">Projects</a>
         </li>
         <li>
           <a href="#" class="block py-2 pl-3 pr-4 text-purple-700 md:hover:text-orange-700 md:p-0 ">About</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 pl-3 pr-4 text-purple-700 md:hover:text-orange-700 md:p-0 ">Blog</a>
         </li>
         <li>
           <a href="#" class="block py-2 pl-3 pr-4 text-purple-700 md:hover:text-orange-700 md:p-0 ">Contact</a>
