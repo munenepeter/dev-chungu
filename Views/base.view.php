e474e6a600f4cb8226c9bb9572112e5f35d49a6d
munenepeter <peternjega7@gmail.com>
2023-03-29 16:00:07 +0300

    update font-family to Ubuntu

--- Views/base.view.php
+++ Views/base.view.php
@@ -9,10 +9,19 @@
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio"></script>
     <link rel="icon" href="/../static/imgs/favicon/norm-favicon.svg" type="image/svg">
     <link rel="icon" href="<?php asset("imgs/favicon/norm-favicon.svg") ?>" type="image/svg">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/2.3.0/alpine.js" integrity="sha512-nIwdJlD5/vHj23CbO2iHCXtsqzdTTx3e3uAmpTm4x2Y8xCIFyWu4cSIV8GaGe2UNVq86/1h9EgUZy7tn243qdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
     <script src="https://unpkg.com/flowbite@1.5.5/dist/flowbite.js"></script>
+
 </head>
+<style>
+    * {
+        font-family: 'Ubuntu', sans-serif;
+    }
+</style>
 
 <body class="bg-purple-50">
