302fd4f32509f14913c484d67169838962c18d58
munenepeter <peternjega7@gmail.com>
2023-04-13 16:07:24 +0300

    uplift to new format

--- Views/get-links.view.php
+++ Views/get-links.view.php
@@ -5,35 +5,6 @@ use spekulatius\phpscraper;
 include_once 'base.view.php';
 include_once 'sections/nav.view.php';
 ?>
-<nav class="pt-2 pb-2 mb-2 bg-gray-50 border border-gray-100 px-2 sm:px-4 py-2.5 rounded">
-       <div class="container flex flex-wrap justify-between items-center mx-auto">
-              <div class="m-auto w-full text-center md:w-auto text-blue-700 text-3xl">
-                     Get all the urls of a Webpage
-              </div>
-       </div>
-</nav>
-
-<div class="m-4 md:m-auto p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-       <h1 class="mb-2 text-2xl font-bold tracking-tight">
-              <?php if (isset($_GET['error'])) : ?>
-                     <span class="text-red-500"><?= $_GET['error'] ?>
-                     </span>
-              <?php endif; ?>
-       </h1>
-       <form action="" method="get">
-              <div class="mb-6">
-                     <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Enter a URL</label>
-                     <input type="url" name="url" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-                     <div class="my-4 text-sm text-center text-gray-500">Only links to actual websites and/or webpages will work</div>
-
-              </div>
-              <center>
-                     <button name="submit" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Search</button>
-
-              </center>
-       </form>
-</div>
-
 
 <?php
 
@@ -59,19 +30,54 @@ if (isset($_GET['submit'])) {
        }
 
 ?>
+
+       <main class="mt-4 pt-20">
+              <section class="flex justify-between px-2 mx-auto max-w-screen-2xl ">
+                     <article class="mx-auto w-full max-w-4xl format format-sm sm:format-base lg:format-lg format-purple dark:format-invert">
+                            <div class=" p-4 rounded-lg bg-gray-100 dark:bg-gray-800"">
+                              <p class=" text-lg text-center font-medium text-gray-900 dark:text-white">Get all the urls of a Webpage</p>
+                                   <label id="url-label-guide" class="block mb-2 text-sm text-center text-gray-500 dark:text-gray-400" for="url">Only links to actual websites and/or webpages will work</label>
+                                   <form action="" method="get" class="flex flex-row justify-center items-center">
+                                          <input name="url" type="url" id="url-input" class="mr-2 block w-3/4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-2.5 " placeholder="https://example.com" />
+                                          <button name="submit" type="submit" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 ">Search</button>
+                                   </form>
+                                   <section class="mt-4 p-2 rounded-lg bg-white">
+                                          <?php if (isset($_GET['error'])) : ?>
+                                                 <span class="text-red-500"><?= $_GET['error'] ?>
+                                                 </span>
+                                          <?php endif; ?>
+                                          <div class="flex justify-between items-center p-2">
+                                                 <p class="text-md"><?= "Done getting " . count($web->links) . " links for <i>$host</i> in <b>$time</b> Seconds"; ?></p>
+                                                 <button onclick="CopyToClipboard('div1')" class="text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-3 py-2 text-center" disabled>Copy Links</button>
+                                          </div>
+
+                                          <div class="p-2 overflow-y-auto h-96">
+                                                 <?php $count = 1; ?>
+                                                 <?php foreach ($links as $link) : ?>
+                                                        <p class='font-normal text-gray-700'><a class="text-blue-600 hover:underline" href="<?= $link; ?>"><?= $link; ?></a></p>
+                                                        <?php $count++; ?>
+                                                 <?php endforeach; ?>
+
+                                          </div>
+                                   </section>
+                                   <section class="mt-2 flex justify-between">
+                                          <div class="hidden bg-gray-50 rounded-lg">
+                                          </div>
+                                   </section>
+                            </div>
+
+                     </article>
+              </section>
+       </main>
+
+
+
+
+
        <div>
-              <div class="flex justify-between items-center p-2">
-                     <p class="text-lg"><?= "Done getting " . count($web->links) . " links for <i>$host</i> in <b>$time</b> Seconds"; ?></p>
-                     <button onclick="CopyToClipboard('div1')" class="text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center" disabled>Copy Links</button>
-              </div>
-              <span id="div1" class="hidden"><?= implode("\n", $links); ?></span>
-              <div class="border m-4 p-2 rounded-md h-56 overflow-y-auto">
-                     <?php $count = 1; ?>
-                     <?php foreach ($links as $link) : ?>
-                            <p class='font-normal text-gray-700'><a class="text-blue-600 hover:underline" href="<?= $link; ?>"><?= $link; ?></a></p>
-                            <?php $count++; ?>
-                     <?php endforeach; ?>
-              </div>
+
+
+
        </div>
 
 
@@ -106,9 +112,9 @@ if (isset($_GET['submit'])) {
 }
 ?>
 <div class="border-t bg-gray-50 left-50 w-full  bottom-0" style="position: fixed;  left: 50%; transform: translate(-50%, 0);">
-    <div class="text-gray-900 text-sm text-center">
-        <div class="my-5 text-center">&copy; 2020 - <?= date('Y') ?> All rights reserved | Chungu Developers</div>
-    </div>
+       <div class="text-gray-900 text-sm text-center">
+              <div class="my-5 text-center">&copy; 2020 - <?= date('Y') ?> All rights reserved | Chungu Developers</div>
+       </div>
 </div>
 </body>
 
