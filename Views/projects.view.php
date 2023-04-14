18a6496884e55f68360dec89882cbd68ba1f169c
munenepeter <peternjega7@gmail.com>
2023-04-14 19:44:36 +0300

    added a gradient

--- Views/projects.view.php
+++ Views/projects.view.php
@@ -2,8 +2,8 @@
 include_once 'base.view.php';
 include_once 'sections/nav.view.php';
 ?>
-<main class="py-6 my-8">
-       <section class="mx-auto max-w-screen-2xl mt-6 px-4">
+<main class="py-8 my-8 bg-gradient-to-r from-yellow-50 from-2% via-green-100 via-40% to-pink-100 to-90%">
+       <section class="mx-auto max-w-screen-2xl mt-6 px-4 ">
               <div class="flex items-center space-x-2 text-orange-800">
                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
@@ -13,7 +13,7 @@ include_once 'sections/nav.view.php';
               </div>
               <div class="mt-6 grid md:grid-cols-4 grid-cols-1 mt-2 gap-2 mx-auto">
                      <?php foreach ($projects as $project) : ?>
-                            <div class=" shadow-sm border border-purple-100 hover:border-purple-200   p-4 rounded-md hover:bg-purple-100 hover:shadow-md">
+                            <div class="bg-gray-50 shadow-sm border border-purple-100 hover:border-purple-200 p-4 rounded-md hover:bg-purple-50 hover:shadow-md">
                                    <div class="flex justify-between items-center mb-4">
                                           <div class="flex items-center space-x-2">
                                                  <div class="border border-purple-100 p-1 rounded-md">
@@ -26,7 +26,7 @@ include_once 'sections/nav.view.php';
                                                         <p><a class="text-purple-900 hover:underline font-bold" href="projects/<?= $project['category'] ?>/<?= $project['name'] ?>"><?= $project['name'] ?></a></p>
                                                  </div>
                                           </div>
-                                          <div class="hidden hover:inline-flex">
+                                          <div class="">
                                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                                  </svg>
