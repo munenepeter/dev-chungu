fb7aafdfe252c26805d69afe71c29300974fe250
munenepeter <peternjega7@gmail.com>
2023-04-14 18:27:36 +0300

    all done with the uplift

--- Views/index.view.php
+++ Views/index.view.php
@@ -49,7 +49,7 @@ include_once 'sections/nav.view.php';
 
 
 
-<section class="p-6 bg-purple-50 text-gray-900 shadow-2xl border border-purple-50">
+<section class="p-6 bg-purple-50 text-gray-900 shadow-lg border border-purple-50">
     <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
         <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
--- Views/projects.view.php
+++ Views/projects.view.php
@@ -3,20 +3,20 @@ include_once 'base.view.php';
 include_once 'sections/nav.view.php';
 ?>
 <main class="py-6 my-8">
-       <section class="mx-auto max-w-screen-2xl mt-6 px-2">
-              <div class="flex items-center space-x-4 my-4">
+       <section class="mx-auto max-w-screen-2xl mt-6 px-4">
+              <div class="flex items-center space-x-2 text-orange-800">
                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                      </svg>
 
-                     <h2>All Projects</h2>
+                     <h2 class="font-bold text-lg ">All projects</h2>
               </div>
               <div class="mt-6 grid md:grid-cols-4 grid-cols-1 mt-2 gap-2 mx-auto">
                      <?php foreach ($projects as $project) : ?>
-                            <div class=" shadow-sm border border-purple-100   p-4 rounded-md hover:bg-purple-100 hover:shadow-md">
+                            <div class=" shadow-sm border border-purple-100 hover:border-purple-200   p-4 rounded-md hover:bg-purple-100 hover:shadow-md">
                                    <div class="flex justify-between items-center mb-4">
                                           <div class="flex items-center space-x-2">
-                                                 <div class="border border-purple-100 p-1">
+                                                 <div class="border border-purple-100 p-1 rounded-md">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
                                                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
                                                         </svg>
@@ -26,10 +26,11 @@ include_once 'sections/nav.view.php';
                                                         <p><a class="text-purple-900 hover:underline font-bold" href="projects/<?= $project['category'] ?>/<?= $project['name'] ?>"><?= $project['name'] ?></a></p>
                                                  </div>
                                           </div>
-                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                                          </svg>
-
+                                          <div class="hidden hover:inline-flex">
+                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                                                 </svg>
+                                          </div>
                                    </div>
                                    <p class="text-xs text-gray-500"><?= $project['description'] ?></p>
                             </div>
