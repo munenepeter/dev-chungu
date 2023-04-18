c9942d6f5bf3222b234032c7f66c9eed517b1725
munenepeter <peternjega7@gmail.com>
2023-04-18 15:02:40 +0300

    add nav with color

--- Views/projects.view.php
+++ Views/projects.view.php
@@ -2,15 +2,23 @@
 include_once 'base.view.php';
 include_once 'sections/nav.view.php';
 ?>
-<main class="bg-gradient-to-r from-yellow-50 from-2% via-green-100 via-40% to-pink-100 to-90% h-screen ">
-       <section class="py-8 mt-8">
-              <article class="mx-auto max-w-screen-2xl mt-6 px-4">
-                     <div class="flex items-center space-x-2 text-orange-800">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                   <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                            </svg>
+<main class="-mt-12 bg-gradient-to-r from-yellow-50 from-2% via-green-100 via-40% to-pink-100 to-90% h-screen ">
+       <section class="py-8 mt-12">
+              <article class="mx-auto max-w-screen-2xl mt-12 px-4">
+                     <div class="flex justify-between">
+                            <div class="flex items-center space-x-2 text-orange-800">
+                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                                   </svg>
 
-                            <h2 class="font-bold text-lg ">All projects</h2>
+                                   <h2 class="font-bold text-lg ">All projects</h2>
+                            </div>
+                            <div class="flex items-center space-x-2 text-orange-800">
+                                   <h2 class="font-bold text-lg ">View github</h2>
+                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                   </svg>
+                            </div>
                      </div>
                      <div class="mt-6 grid md:grid-cols-4 grid-cols-1 mt-2 gap-2 mx-auto">
                             <?php foreach ($projects as $project) : ?>
--- Views/sections/nav.view.php
+++ Views/sections/nav.view.php
@@ -1,11 +1,6 @@
-<nav class="px-2 sm:px-4 py-2 fixed w-full  top-0 left-0 backdrop-blur-3xl">
+<nav class="px-2 sm:px-4 py-2 fixed w-full  top-0 left-0 backdrop-blur-3xl shadow-sm bg-rose-50">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
     <div class="flex items-center space-x-2">
-      <!-- <div class="h-8 w-8 bg-orange-200 rounded-full">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full p-1 text-orange-700">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-        </svg>
-      </div> -->
       <div class="h-8 w-8 bg-orange-200 rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-full h-full p-1 text-orange-700">
           <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
