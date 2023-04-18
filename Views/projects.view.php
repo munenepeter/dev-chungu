365ff757ce7d86f19d24014a20853805aed2a180
munenepeter <peternjega7@gmail.com>
2023-04-18 15:09:36 +0300

    change form All to Home & simplify github

--- Views/projects.view.php
+++ Views/projects.view.php
@@ -4,17 +4,17 @@ include_once 'sections/nav.view.php';
 ?>
 <main class="-mt-12 bg-gradient-to-r from-yellow-50 from-2% via-green-100 via-40% to-pink-100 to-90% h-screen ">
        <section class="py-8 mt-12">
-              <article class="mx-auto max-w-screen-2xl mt-12 px-4">
+              <article class="mx-auto max-w-screen-2xl md:mt-20 mt-12 px-4">
                      <div class="flex justify-between">
                             <div class="flex items-center space-x-2 text-orange-800">
                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                           <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                                    </svg>
 
-                                   <h2 class="font-bold text-lg ">All projects</h2>
+                                   <h2 class="font-bold text-lg ">Home</h2>
                             </div>
                             <div class="flex items-center space-x-2 text-orange-800">
-                                   <h2 class="font-bold text-lg ">View github</h2>
+                                   <h2 class="font-bold text-lg ">Github</h2>
                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                           <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                    </svg>
