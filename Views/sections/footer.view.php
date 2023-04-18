bae0861f9590a11d61f2d4d0875bfb175ed1c000
munenepeter <peternjega7@gmail.com>
2023-04-18 15:46:41 +0300

    optimize for mobile

--- Views/sections/footer.view.php
+++ Views/sections/footer.view.php
@@ -1,22 +1,22 @@
 
 <section id="footer">
-        <div class="max-w-screen-lg px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-5 sm:grid-cols-2 mx-auto">
+        <div class="max-w-screen-lg px-4 sm:px-6 text-gray-800 grid md:grid-cols-5 grid-cols-2 mx-auto">
             <div class="p-4">
-                <p style="font-family: 'Cedarville Cursive', cursive;" class="text-purple-900 ml-2 hover:text-orange-700 text-4xl font-black dark:text-white">Chungu</p>
-                <p class="text-purple-900 text-lg font-bold whitespace-nowrap dark:text-white hover:text-orange-700">Developers</p>
+                <p style="font-family: 'Cedarville Cursive', cursive;" class="text-purple-900 ml-2 hover:text-orange-700 text-4xl font-black">Chungu</p>
+                <p class="text-purple-900 text-lg font-bold whitespace-nowrap hover:text-orange-700">Developers</p>
             </div>
 
-            <div class="p-4">
+            <div class="md:p-4 p-2">
                 <div class="text-sm uppercase text-orange-700 font-bold">Useful Links</div>
                 <a class="my-3 block text-purple-900 hover:underline" href="/services">Services<span class="text-teal-900 text-xs p-1"></span></a>
                 <a class="my-3 block text-purple-900 hover:underline" href="/projects">Projects<span class="text-teal-900 text-xs p-1"></span></a>
             </div>
-            <div class="p-4">
+            <div class="md:p-4 p-2">
                 <div class="text-sm uppercase text-orange-700 font-bold">Support</div>
                 <a class="my-3 block text-purple-900 hover:underline" href="/help-center">Help Center <span class="text-teal-900 text-xs p-1"></span></a>
                 <a class="my-3 block text-purple-900 hover:underline" href="https://chungu.co.ke/privacy-policy">Privacy Policy <span class="text-teal-900 text-xs p-1"></span></a>
             </div>
-            <div class="p-4">
+            <div class="md:p-4 p-2">
                 <div class="text-sm uppercase text-orange-700 font-bold">Contact us</div>
                 <a class="my-3 block text-purple-900 hover:underline" href="/#">+2547 426 41376<br>+2547 980 55244<br> Nairobi, Kenya. <span class="text-teal-900 text-xs p-1"></span></a>
             </div>
