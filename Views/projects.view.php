528a2728cbe4faa4a8114cf3fe44eaabbad00f89
munenepeter <peternjega7@gmail.com>
2023-04-18 14:25:52 +0300

    doen with projects

--- Views/projects.view.php
+++ Views/projects.view.php
@@ -14,11 +14,11 @@ include_once 'sections/nav.view.php';
                      </div>
                      <div class="mt-6 grid md:grid-cols-4 grid-cols-1 mt-2 gap-2 mx-auto">
                             <?php foreach ($projects as $project) : ?>
-                                   <div class="bg-gray-50 shadow-sm border border-rose-100 hover:border-rose-200 p-4 rounded-md hover:bg-purple-50 hover:shadow-md">
+                                   <div class="bg-gray-50 shadow-sm border border-rose-100 hover:border-rose-300 p-4 rounded-md hover:bg-purple-50 hover:shadow-md">
                                           <div class="flex justify-between items-center mb-4">
                                                  <div class="flex items-center space-x-2">
                                                         <div class="border border-purple-100 p-1 rounded-md">
-                                                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10">
+                                                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 text-rose-900">
                                                                       <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
                                                                </svg>
                                                         </div>
