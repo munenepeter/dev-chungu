96b7c264e229810f675eedaf5201c3b89caf9ed5
munenepeter <peternjega7@gmail.com>
2023-10-26 14:07:45 +0300

    update routes to ClientQoute

--- routes.php
+++ routes.php
@@ -7,7 +7,7 @@ $router->get('', 'PagesController@index');
 $router->get('projects', 'PagesController@projects');
 $router->get('services', 'PagesController@services');
 
-$router->post('index/intent/sendqoute', 'QouteController@sendQoute');
+$router->post('index/intent/sendqoute', 'ClientQouteController@sendQoute');
 
 
 //scrapword
