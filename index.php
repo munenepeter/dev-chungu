5e003a52cc9c067d77c72e44beb783df6266c03e
munenepeter <peternjega7@gmail.com>
2023-11-22 11:35:09 +0300

    trying to install cs-fixer

--- index.php
+++ index.php
@@ -8,11 +8,7 @@ require 'Core/bootstrap.php';
 
 //Try to load the routes, direct the URI and check the request method
  try {
-
     Router::load('routes.php')->direct(Request::uri(), Request::method());
-
 } catch (\Exception $e) {
-
-    //Instead of catching the exception here we redirect the same to our main error handler
     abort($e->getMessage(), $e->getCode());
 }
