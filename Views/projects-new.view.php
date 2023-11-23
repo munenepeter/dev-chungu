978dc1a5673f6c2a279711213a32847da8e33c8d
munenepeter <peternjega7@gmail.com>
2023-11-23 15:38:45 +0300

    migrate the top section

--- Views/projects-new.view.php
+++ Views/projects-new.view.php
@@ -4,26 +4,41 @@ include_once 'sections/nav.view.php';
 ?>
 <main class="-mt-24 bg-gradient-to-r from-yellow-50 from-2% via-green-100 via-40% to-pink-100 to-90% h-screen ">
 
-       <section class="py-8 mt-16">
+       <section class="py-8 mt-20">
               <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                      <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-                            <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 ">Our Projects</h2>
+                            <h2 class="my-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-orange-800 ">Our Projects</h2>
                             <p class="font-light text-gray-500 sm:text-xl ">Chungu Developers helps you achiev your business goals faster</p>
+                            <div class="flex justify-center gap-2">
+                                   <div class="flex items-center space-x-2 text-orange-800">
+                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                          </svg>
+                                          <a href="https://github.com/munenepeter" class="font-bold text-lg ">View all projects</a>
+                                   </div>
+                                   <div class="flex items-center space-x-2 text-orange-800">
+                                          <a href="" class="font-bold text-lg ">Let's work together</a>
+                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                          </svg>
+                                   </div>
+                            </div>
                      </div>
                      <div class="grid gap-8 lg:grid-cols-3">
 
                             <?php for ($i = 0; $i < 6; $i++) : ?>
-                                   <article class="max-w-sm bg-white rounded-lg">
+                                   <article class="max-w-sm rounded-lg">
                                           <a href="#">
-                                                 <img class="rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
+                                                 <img class="rounded-lg h-56 w-full" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
                                           </a>
-                                          <div class="px-2 pt-5">
+                                          <div class="px-2 pt-5 pb-2 bg-gray-50 shadow-sm border border-rose-100 hover:border-rose-300 rounded-md hover:bg-purple-50 hover:shadow-md">
                                                  <a href="#">
-                                                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Project 1</h5>
+                                                        <h5 class="mb-2 text-purple-900 hover:underline font-bold">Project <?= $i ?></h5>
                                                  </a>
-                                                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                                                 <p class="mb-3 text-sm text-gray-500">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
 
-                                                 <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                 <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">
                                                         Live Preview
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ms-2">
                                                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
