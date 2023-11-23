827c1f1b7edab70b8a1e292e965aa875a54a1e18
munenepeter <peternjega7@gmail.com>
2023-11-23 16:09:50 +0300

    move to the new UI

--- Controllers/ProjectsController.php
+++ Controllers/ProjectsController.php
@@ -5,6 +5,22 @@ namespace Chungu\Controllers;
 class ProjectsController extends Controller {
 
     public function index() {
-        return view('projects-new');
+
+
+        $projects = [
+            ['category' => 'automation', 'name' => "scrapword", 'description' => 'Searching for Keywords on a webpage'],
+            ['category' => 'automation', 'name' => "excel-to-json", 'description' => 'Convert an Excel doc to JSON'],
+            ['category' => 'automation', 'name' => "parser", 'description' => 'Parse docx, pdf, rtf & txt files'],
+            ['category' => 'automation', 'name' => "get-links", 'description' => 'Get all the urls of a webpage'],
+            ['category' => 'automation', 'name' => "leg-initia", 'description' => 'Search Legislative initiatives'],
+            ['category' => 'learning', 'name' => "api", 'description' => 'Demo API endpoints for Devs talk'],
+            ['category' => 'law', 'name' => "case-law-search", 'description' => 'A simple Case Search with highlights'],
+            ['category' => 'business', 'name' => "board-room", 'description' => 'A board room scheduling mini-system'],
+            ['category' => 'business', 'name' => "poems", 'description' => 'All my poems in one place :)'],
+
+        ];
+        return view('projects', [
+            'projects' => $projects
+        ]);
     }
 }
--- Views/projects.view.php
+++ Views/projects.view.php
@@ -2,57 +2,59 @@
 include_once 'base.view.php';
 include_once 'sections/nav.view.php';
 ?>
-<main class="-mt-12 bg-gradient-to-r from-yellow-50 from-2% via-green-100 via-40% to-pink-100 to-90% h-screen ">
-       <section class="py-8 mt-12">
-              <article class="mx-auto max-w-screen-2xl md:mt-20 mt-12 px-4">
-                     <div class="flex justify-between">
-                            <div class="flex items-center space-x-2 text-orange-800">
-                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                                   </svg>
+<main class="-mt-24 bg-gradient-to-r from-yellow-50 from-2% via-green-100 via-40% to-pink-100 to-90%">
 
-                                   <a href="/" class="font-bold text-lg ">Home</a>
-                            </div>
-                            <div class="flex items-center space-x-2 text-orange-800">
-                                   <a href="https://github.com/munenepeter" class="font-bold text-lg ">Github</a>
-                                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                   </svg>
+       <section class="py-8 mt-20">
+              <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                     <div class="mx-auto max-w-screen-sm text-center lg:mb-8 mb-4">
+                            <h2 class="my-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-orange-800 ">Our Projects</h2>
+                            <p class="font-light text-gray-500 sm:text-xl ">Chungu Developers helps you achiev your business goals faster</p>
+                            <div class="flex justify-center gap-6 mt-2">
+                                   <div class="flex items-center space-x-2 text-purple-900">
+                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                          </svg>
+                                          <a href="https://github.com/munenepeter" class="font-bold text-md ">View all projects</a>
+                                   </div>
+                                   <div class="flex items-center space-x-2 text-purple-900">
+                                          <a href="/#contact" class="font-bold text-md ">Let's work together</a>
+                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                                          </svg>
+
+                                   </div>
                             </div>
                      </div>
-                     <div class="mt-6 grid md:grid-cols-4 grid-cols-1 gap-2 mx-auto">
-                            <?php foreach ($projects as $project) : ?>
-                                   <div class="bg-gray-50 shadow-sm border border-rose-100 hover:border-rose-300 p-4 rounded-md hover:bg-purple-50 hover:shadow-md">
-                                          <div class="flex justify-between items-center mb-4">
-                                                 <div class="flex items-center space-x-2">
-                                                        <div class="border border-purple-100 p-1 rounded-md">
-                                                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 text-rose-900">
-                                                                      <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
-                                                               </svg>
-                                                        </div>
-                                                        <div>
-                                                               <p class="text-xs text-orange-600"><?= $project['category'] ?></p>
-                                                               <p><a class="text-purple-900 hover:underline font-bold" href="projects/<?= $project['category'] ?>/<?= $project['name'] ?>"><?= $project['name'] ?></a></p>
-                                                        </div>
-                                                 </div>
-                                                 <div class="">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                                               <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM18.75 12a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                                                        </svg>
-                                                 </div>
-                                          </div>
-                                          <p class="text-xs text-gray-500"><?= $project['description'] ?></p>
-                                   </div>
+                     <div class="grid gap-8 lg:grid-cols-3">
 
+                            <?php foreach ($projects as $project) : ?>
+                                   <article class="max-w-sm rounded-lg hover:shadow-md">
+                                          <a href="#">
+                                                 <img class="rounded-lg h-56 w-full shadow-md" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
+                                          </a>
+                                          <div class="p-2 mt-2 border-b border-rose-100 hover:border-rose-300 rounded-b-md hover:bg-purple-50">
+                                                 <p class="mb-3"><span class=" bg-orange-200 p-1 rounded-md text-orange-700 text-xs"><?= $project['category'] ?></span></p>
+                                                 <a href="#">
+                                                        <h5 class="mb-2 text-purple-900 hover:underline font-bold"><?= $project['name'] ?></h5>
+                                                 </a>
+                                                 <p class="mb-3 text-sm text-gray-500"><?= $project['description'] ?></p>
+
+                                                 <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">
+                                                        Live Preview
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ms-2">
+                                                               <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                                        </svg>
+                                                 </a>
+                                          </div>
+                                   </article>
                             <?php endforeach; ?>
 
 
                      </div>
-                     <!-- <h2 class=" text-xl text-center title-font font-medium text-purple-900 mt-8">For More projects you can visit my <a class="text-indigo-500" href="https://github.com/munenepeter">Github</a> page</h2> -->
-
-
-              </article>
+              </div>
        </section>
+
        <div class="border-t bg-gray-50 left-50 w-full  bottom-0" style="position: fixed;  left: 50%; transform: translate(-50%, 0);">
               <div class="text-gray-900 text-sm text-center">
                      <div class="my-2 text-center">&copy; 2020 - <?= date('Y') ?> All rights reserved | Chungu Developers</div>
