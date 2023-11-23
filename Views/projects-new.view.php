8af0f096d1bea685d1adbc1380792a29bab83148
munenepeter <peternjega7@gmail.com>
2023-11-23 15:17:25 +0300

    new ui for projects

--- Views/projects-new.view.php
+++ Views/projects-new.view.php
@@ -7,65 +7,32 @@ include_once 'sections/nav.view.php';
        <section class="py-8 mt-16 bg-white ">
               <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                      <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-                            <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 ">Our Blog</h2>
-                            <p class="font-light text-gray-500 sm:text-xl ">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
+                            <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 ">Our Projects</h2>
+                            <p class="font-light text-gray-500 sm:text-xl ">Chungu Developers helps you achiev your business goals faster</p>
                      </div>
                      <div class="grid gap-8 lg:grid-cols-2">
-                            <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md  ">
-                                   <div class="flex justify-between items-center mb-5 text-gray-500">
-                                          <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded  ">
-                                                 <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-                                                 </svg>
-                                                 Tutorial
-                                          </span>
-                                          <span class="text-sm">14 days ago</span>
-                                   </div>
-                                   <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "><a href="#">How to quickly deploy a static website</a></h2>
-                                   <p class="mb-5 font-light text-gray-500 ">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
-                                   <div class="flex justify-between items-center">
-                                          <div class="flex items-center space-x-4">
-                                                 <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
-                                                 <span class="font-medium ">
-                                                        Jese Leos
-                                                 </span>
-                                          </div>
-                                          <a href="#" class="inline-flex items-center font-medium text-primary-600  hover:underline">
-                                                 Read more
-                                                 <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+
+
+                            <article class="max-w-sm bg-white rounded-lg">
+                                   <a href="#">
+                                          <img class="rounded-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
+                                   </a>
+                                   <div class="pt-5">
+                                          <a href="#">
+                                                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Project 1</h5>
+                                          </a>
+                                          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+
+                                          <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                 Live Preview
+                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ms-2">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                                                  </svg>
                                           </a>
                                    </div>
                             </article>
-                            <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md  ">
-                                   <div class="flex justify-between items-center mb-5 text-gray-500">
-                                          <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded  ">
-                                                 <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path>
-                                                        <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
-                                                 </svg>
-                                                 Article
-                                          </span>
-                                          <span class="text-sm">14 days ago</span>
-                                   </div>
-                                   <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "><a href="#">Our first project with React</a></h2>
-                                   <p class="mb-5 font-light text-gray-500 ">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
-                                   <div class="flex justify-between items-center">
-                                          <div class="flex items-center space-x-4">
-                                                 <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
-                                                 <span class="font-medium ">
-                                                        Bonnie Green
-                                                 </span>
-                                          </div>
-                                          <a href="#" class="inline-flex items-center font-medium text-primary-600  hover:underline">
-                                                 Read more
-                                                 <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                                                 </svg>
-                                          </a>
-                                   </div>
-                            </article>
+
+
                      </div>
               </div>
        </section>
